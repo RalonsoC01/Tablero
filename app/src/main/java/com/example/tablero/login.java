2b1443d663b8b6186c3f10ff8fd58929ac4ee7df
DAM202 <ralonsoc01@educantabria.es>
2022-01-24 16:39:19 +0100

    Rediseño de la interfaz

--- app/src/main/java/com/example/tablero/login.java
+++ app/src/main/java/com/example/tablero/login.java
@@ -2,7 +2,9 @@ package com.example.tablero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void Registro (View view)
+    {
+        Intent i= new Intent(this,Registro.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.deslizar_derecha, R.anim.deslizar_hacia_izquierda);
     }
 }
