6c8754efb0d2261f83813e1533b9e25e176b9b92
DAM202 <ralonsoc01@educantabria.es>
2022-01-25 16:38:48 +0100

    Login OK Iniciamos el registro. Incluimos firebase.

--- app/src/main/java/com/example/tablero/Registro.java
+++ app/src/main/java/com/example/tablero/Registro.java
@@ -1,14 +1,106 @@
 package com.example.tablero;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Registro extends AppCompatActivity {
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Registro extends AppCompatActivity implements View.OnClickListener {
+    private EditText nombre;
+    private EditText email;
+    private EditText contrasena;
+    private FirebaseAuth mAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        nombre=(EditText) findViewById(R.id.txtNombreRegistro);
+        email=(EditText) findViewById(R.id.txtEmail);
+        contrasena=(EditText)findViewById(R.id.txtContrasena);
+
+
+        progressDialog=new ProgressDialog(this);
+    }
+
+    public void registrousuario(View view) {
+
+        String txtEmail = email.getText().toString().trim();
+        String txtNombre = nombre.getText().toString().trim();
+        String txtContrasena = contrasena.getText().toString().trim();
+
+
+        if (TextUtils.isEmpty(txtEmail)) {
+            Toast.makeText(this, "Se debe ingresar un correo", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(txtContrasena)) {
+            Toast.makeText(this, "Falta ingresar la contraseña", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(txtNombre)) {
+            Toast.makeText(this, "Falta ingresar el nombre", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("Registrando...");
+        progressDialog.show();
+        mAuth.createUserWithEmailAndPassword(txtEmail, txtContrasena)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            usuario usuario = new usuario(txtNombre, txtEmail); //Creacion de un objeto de tipo usuario
+
+
+                            FirebaseDatabase.getInstance().getReference("Usuario")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(usuario).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(Registro.this, "Se ha registrado el usuario con el email: " + txtEmail, Toast.LENGTH_SHORT).show();
+
+                                    } else {
+                                        if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                            Toast.makeText(getApplicationContext(), "El usuario ya existe", Toast.LENGTH_SHORT).show();
+                                            Intent inicio = new Intent(getApplication(), login.class);
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "No se pudo registrar el usuario", Toast.LENGTH_SHORT).show();
+                                        }
+                                        progressDialog.dismiss();
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- app/src/main/java/com/example/tablero/login.java
+++ app/src/main/java/com/example/tablero/login.java
@@ -1,22 +1,74 @@
 package com.example.tablero;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class login extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth mAuth;
+    private EditText usuario;
+    private EditText contrasena;
+    private String usuarioStr;
+    private String contrsenaStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_login);
+
+        usuario = (EditText) findViewById(R.id.txtEmail);
+        contrasena = (EditText) findViewById(R.id.txtContrasena);
+
     }
-    public void Registro (View view)
-    {
-        Intent i= new Intent(this,Registro.class);
+
+    public void Registro(View view) {
+        Intent i = new Intent(this, Registro.class);
         startActivity(i);
         overridePendingTransition(R.anim.deslizar_derecha, R.anim.deslizar_hacia_izquierda);
+    }
+
+    public void pasarIndex(View view) {
+
+        usuarioStr = usuario.getText().toString();
+        contrsenaStr = contrasena.getText().toString();
+        if (!usuarioStr.isEmpty() && !contrsenaStr.isEmpty()) {
+            loginUser();
+        } else {
+            Toast.makeText(login.this, "El correo o la contraseña no existe.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void loginUser() {
+        mAuth.signInWithEmailAndPassword(usuarioStr, contrsenaStr).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(login.this, Juego.class);
+                    startActivity(intent);
+                    //Finalizar Activity
+                    finish();
+                } else {
+                    Toast.makeText(login.this, "El correo o la contraseña no existe.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
